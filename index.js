9564cd7234eb4ff00f343d700187398aa106b73f
Shakhzod <ayubjonovshakhzod01@gmail.com>
2020-12-26 23:50:53 +0500

    sda saddas

--- index.js
+++ index.js
@@ -69,28 +69,28 @@ bot.start((ctx) => {
   ctx.reply(
     "Hey there!\nI'm sending Youtube videos to you!"
   );
-  const url = 'https://www.youtube.com/watch?v=pahO5XjnfLA';
+  // const url = 'https://www.youtube.com/watch?v=pahO5XjnfLA';
   
-  const video = ytdl(url, { quality: 'highest' });
-  let starttime;
-  video.pipe(fs.createWriteStream(`${__dirname}/cache/video.mp4`));
-  video.once('response', () => {
-    starttime = Date.now();
-  });
-  video.on('progress', (chunkLength, downloaded, total) => {
-    const percent = downloaded / total;
-    const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
-    const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
-    readline.cursorTo(process.stdout, 0);
-    process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-    process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
-    process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
-    process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
-    readline.moveCursor(process.stdout, 0, -1);
-  });
-  video.on('end', () => {
-    process.stdout.write('\n\n');
-  });
+  // const video = ytdl(url, { quality: 'highest' });
+  // let starttime;
+  // video.pipe(fs.createWriteStream(`${__dirname}/cache/video.mp4`));
+  // video.once('response', () => {
+  //   starttime = Date.now();
+  // });
+  // video.on('progress', (chunkLength, downloaded, total) => {
+  //   const percent = downloaded / total;
+  //   const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+  //   const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
+  //   readline.cursorTo(process.stdout, 0);
+  //   process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+  //   process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
+  //   process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
+  //   process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
+  //   readline.moveCursor(process.stdout, 0, -1);
+  // });
+  // video.on('end', () => {
+  //   process.stdout.write('\n\n');
+  // });
 });
 bot.help((ctx) =>
   ctx.reply(
